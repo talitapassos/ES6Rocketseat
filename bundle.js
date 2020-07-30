bd66073a84dd60a173bfe1d9b7ced66fbc0a2b83
Talita Passos <tata.ogp@gmail.com>
2020-07-30 16:18:59 -0300

    primeiro commit

---
+++ bundle.js
@@ -0,0 +1,52 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+/*class List {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'Talita';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();*/
+var TodoList = /*#__PURE__*/function () {
+  function TodoList() {
+    _classCallCheck(this, TodoList);
+
+    this.todos = [];
+  }
+
+  _createClass(TodoList, null, [{
+    key: "addTodo",
+    value: function addTodo() {}
+  }]);
+
+  return TodoList;
+}();
---
+++ main.js
@@ -0,0 +1,40 @@
+/*class List {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List {
+    constructor() {
+        super();
+
+        this.usuario = 'Talita';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();*/
+
+class TodoList {
+    constructor() {
+        this.todos = [];
+    }
+
+    static addTodo() {
+        
+    }
+}
